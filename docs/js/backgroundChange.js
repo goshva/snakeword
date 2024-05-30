4a77214f7f98dd61c8025a4e1a565afbaa9890c9
Aramo98 <arammargaryan887@gmail.com>
2024-05-30 23:19:40 +0400

    adding images

--- docs/js/backgroundChange.js
+++ docs/js/backgroundChange.js
@@ -10,7 +10,11 @@ let imagesData = [
     "/backgroundImages/zakat-i-rassvet-1280x853px.jpg",
     "/backgroundImages/zakat-i-rassvet-2000x1333px.jpg",
     "/backgroundImages/zakat-i-rassvet-2056x1365px.jpg",
-    "../../img/bcg.jpg"
+    "../../img/bcg.jpg",
+    "/backgroundImages/depositphotos_403636008-stock-photo-colourful-sunset-beach-atlantic-ocean.jpg",
+    "/backgroundImages/images.jpg",
+    "/backgroundImages/images_2.jpg",
+    "/backgroundImages/images_2.jpg",
 ]
 
 const randomIndex = Math.floor(Math.random() * imagesData.length);
