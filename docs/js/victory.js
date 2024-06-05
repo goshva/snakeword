4ffde2e7017fe5375d46071a5fa2b312113c5c46
Aramo98 <arammargaryan887@gmail.com>
2024-06-05 08:56:09 +0400

    corrections and changes

--- docs/js/victory.js
+++ docs/js/victory.js
@@ -99,17 +99,17 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     
     else{
-      deadline -= 1000;
+      let audio = document.createElement("audio");
+      let audioSrc = document.createElement("source");
+      audioSrc.src = "/audios/mixkit-fast-small-sweep-transition-166.wav";
+            audio.appendChild(audioSrc)
+            audio.play()
+            document.body.appendChild(audio)
+            setTimeout(()=>{
+              audio.pause()
+             },1000)
       timerId = setInterval(()=>{
-        let audio = document.createElement("audio");
-        let audioSrc = document.createElement("source");
-        audioSrc.src = "/audios/mixkit-fast-small-sweep-transition-166.wav";
-              audio.appendChild(audioSrc)
-              audio.play()
-              document.body.appendChild(audio)
-              setTimeout(()=>{
-                audio.pause()
-               },1000)
+     
         const $minutes = document.querySelector('.timer__minutes');
         const $seconds = document.querySelector('.timer__seconds');
         modalBlock.classList.add("active")
@@ -118,6 +118,7 @@ document.addEventListener('DOMContentLoaded', function () {
       const seconds = deadline > 0 ? Math.floor(deadline / 1000) % 60 : 0;
       $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
       $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
+      deadline -= 1000;
       },1000);
       
     // decrease remaining time by one second
