e4d8ba48eff53397d6da8042227839eb2fb43e32
Aramo98 <arammargaryan887@gmail.com>
2024-07-03 12:43:51 +0400

    styling button part 2

--- docs/js/victory.js
+++ docs/js/victory.js
@@ -67,8 +67,6 @@ alert("ds")
         $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
         deadline -= 1000;
         if (minutes === 0 && seconds === 0) {  
-          
-       
           popup.classList.add("active")
             restartBtn.classList.add("active") 
               }
