23db76ff5e8309b2fedd8e08816b9dce7245a077
Aramo98 <arammargaryan887@gmail.com>
2024-07-04 09:42:09 +0400

    correction

--- docs/js/victory.js
+++ docs/js/victory.js
@@ -68,10 +68,7 @@ document.addEventListener('DOMContentLoaded', function () {
         $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
         deadline -= 1000;
         if (minutes === 0 && seconds === 0) {  
-          
-
           popup.classList.add("active")
-            restartBtn.classList.add("active") 
               }
         },1000);
       
