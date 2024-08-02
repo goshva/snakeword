b09f30b4ddccdd11582df7162c880b567b8ed349
Aramo98 <arammargaryan887@gmail.com>
2024-08-02 20:48:52 +0400

    adding function for toggle button

--- themes/snakeword/static/js/backgroundChange.js
+++ themes/snakeword/static/js/backgroundChange.js
@@ -29,7 +29,8 @@ body.appendChild(backgoundImage);
 
 const hamburgerBlockBtn = document.querySelector(".hamburgerBlockBtn");
 const iconBlock = document.querySelector(".iconBlock");
-
+const navigationBar = document.querySelector(".navigationBar")
 hamburgerBlockBtn.addEventListener("click",()=>{
     iconBlock.classList.add("active")
+    navigationBar.classList.add("active")
 })
