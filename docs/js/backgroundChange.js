58bf46f43b0a5ab48373fe10d5c8469e3cd52f19
Aramo98 <arammargaryan887@gmail.com>
2024-08-03 21:16:01 +0400

    delete toggle function

--- docs/js/backgroundChange.js
+++ docs/js/backgroundChange.js
@@ -24,13 +24,5 @@ body.appendChild(backgoundImage);
 
 
 
-// change toggle icons 
 
 
-const hamburgerBlockBtn = document.querySelector(".hamburgerBlockBtn");
-const iconBlock = document.querySelector(".iconBlock");
-const navigationBar = document.querySelector(".navigationBar")
-hamburgerBlockBtn.addEventListener("click",()=>{
-    iconBlock.classList.add("active")
-    navigationBar.classList.add("active")
-})
