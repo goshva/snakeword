f1767c0ed783d49ffacdcc8e15ee68024346c5b8
Aramo98 <arammargaryan887@gmail.com>
2024-08-04 10:13:13 +0400

    correct

--- docs/js/backgroundChange.js
+++ docs/js/backgroundChange.js
@@ -24,5 +24,13 @@ body.appendChild(backgoundImage);
 
 
 
+// change toggle icons 
 
 
+const hamburgerBlockBtn = document.querySelector(".hamburgerBlockBtn");
+const iconBlock = document.querySelector(".iconBlock");
+const navigationBar = document.querySelector(".navigationBar")
+hamburgerBlockBtn.addEventListener("click",()=>{
+    iconBlock.classList.add("active")
+    navigationBar.classList.add("active")
+})
