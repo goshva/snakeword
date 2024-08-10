d506d4dec1c810fffc1fcfd1fbabc64498d8ae70
Aramo98 <arammargaryan887@gmail.com>
2024-08-10 11:45:55 +0400

    correction

--- themes/snakeword/static/js/backgroundChange.js
+++ themes/snakeword/static/js/backgroundChange.js
@@ -23,14 +23,6 @@ backgoundImage.src = imagesData[randomIndex];
 body.appendChild(backgoundImage);
 
 
+ 
 
-// change toggle icons 
 
-
-const hamburgerBlockBtn = document.querySelector(".hamburgerBlockBtn");
-const iconBlock = document.querySelector(".iconBlock");
-const navigationBar = document.querySelector(".navigationBar")
-hamburgerBlockBtn.addEventListener("click",()=>{
-    iconBlock.classList.add("active")
-    navigationBar.classList.add("active")
-})
