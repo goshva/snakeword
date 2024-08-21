84b70d2195df313208dd28db93577194318c1dbe
Aramo98 <arammargaryan887@gmail.com>
2024-08-21 17:47:01 +0400

    make light dark logic

--- docs/js/toggleBlock.js
+++ docs/js/toggleBlock.js
@@ -5,3 +5,13 @@ hamburgerBlockBtn.addEventListener("click",()=>{
     iconBlock.classList.toggle("active")
     navigationBar.classList.toggle("active")
 })
+
+
+// light Dark Toggle
+
+
+const lightBlock = document.querySelector(".lightDarkBlock")
+const toggleCircle = document.querySelector(".toggleCircle");
+ lightBlock.addEventListener("click",()=>{
+    toggleCircle.classList.toggle("active")
+ })
