e3db8473c29d1d8f038f8edfff55e1d63c9ee8ff
Aramo98 <arammargaryan887@gmail.com>
2024-08-22 22:56:51 +0400

    logic for light and dark theme

--- themes/snakeword/static/js/toggleBlock.js
+++ themes/snakeword/static/js/toggleBlock.js
@@ -10,10 +10,63 @@ hamburgerBlockBtn.addEventListener("click",()=>{
 // light Dark Toggle
 
 
-const lightBlock = document.querySelector(".lightDarkBlock")
-const toggleCircle = document.querySelector(".toggleCircle");
-const iconToggle = document.querySelector(".iconToggle")
- lightBlock.addEventListener("click",()=>{
-    toggleCircle.classList.toggle("active");
-  iconToggle.classList.toggle("active")
+
+  const lightBlock = document.querySelectorAll(".lightDarkBlock");
+  const toggleCircle = document.querySelectorAll(".toggleCircle");
+const iconToggle = document.querySelectorAll(".iconToggle");
+ const topMenu = document.querySelector(".top-menu")
+ const top_menu_item =document.querySelectorAll(".top-menu-item");
+ const setting_item = document.querySelector(".setting-item");
+ const modalLight = document.querySelector(".modalBlock");
+ const languageList = document.querySelector(".for_language");
+
+
+ lightBlock.forEach((lightCheck,lightIndex)=>{
+lightCheck.addEventListener("click",()=>{
+  if(localStorage.getItem("themes") === "light"){
+    localStorage.removeItem("themes")
+  }else{
+    localStorage.setItem("themes","light")
+  }
+
+  changeDarkToLight()
+})
+
+function changeDarkToLight(){
+  
+  if (localStorage.getItem("themes") === "light") {
+    lightCheck.classList.add("light")
+    toggleCircle[lightIndex].classList.add("light")
+    iconToggle[lightIndex].classList.add("light");
+   topMenu.classList.add("light")
+    top_menu_item.forEach((item)=>{
+      item.classList.add("light")
+    })
+    setting_item.classList.add("light");
+     modalLight.classList.add("light");
+     languageList.classList.add("light")
+     hamburgerBlockBtn.classList.add("light")
+     navigationBar.classList.add("light")
+  }else{
+    lightCheck.classList.remove("light")
+    toggleCircle[lightIndex].classList.remove("light")
+    iconToggle[lightIndex].classList.remove("light")
+    topMenu.classList.remove("light")
+    top_menu_item.forEach((item)=>{
+      item.classList.remove("light")
+    })
+    setting_item.classList.remove("light");
+    modalLight.classList.remove("light");
+    languageList.classList.remove("light");
+    hamburgerBlockBtn.classList.remove("light")
+    navigationBar.classList.remove("light")
+  }
+}
+
+
+changeDarkToLight()
  })
+
+
+
+
