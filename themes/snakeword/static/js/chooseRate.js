d8d51e7cfb994e979444dbd4e7813c9d834a71f2
Aramo98 <arammargaryan887@gmail.com>
2024-08-29 13:04:53 +0400

    changings

--- themes/snakeword/static/js/chooseRate.js
+++ themes/snakeword/static/js/chooseRate.js
@@ -4,7 +4,7 @@ const rateButtonsBlock = document.querySelector(".rateBtns")
 const choosenBlock = document.querySelector(".choosenBlock");
 const choosen_icon = document.querySelector(".choosen_icon")
 const LS = localStorage
-const storageNum = 0
+let storageNum = 0
 LS.getItem("rateNumber")
 rateButtons.forEach((button,index)=>{
     button.addEventListener("click",()=>{
