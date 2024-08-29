90b7dee5efa2595926a5c4508409b9c39e505568
Aramo98 <arammargaryan887@gmail.com>
2024-08-29 13:35:01 +0400

    adding and correction

--- docs/js/chooseRate.js
+++ docs/js/chooseRate.js
@@ -3,6 +3,8 @@ const rateButtons = document.querySelectorAll(".rateBtn")
 const rateButtonsBlock = document.querySelector(".rateBtns")
 const choosenBlock = document.querySelector(".choosenBlock");
 const choosen_icon = document.querySelector(".choosen_icon")
+const rateHeader = document.querySelector(".rateHeader")
+const ratedText = document.querySelector(".ratedText")
 const LS = localStorage
 let storageNum = 0
 LS.getItem("rateNumber")
@@ -12,6 +14,8 @@ rateButtons.forEach((button,index)=>{
     LS.setItem("rateNumber",storageNum)
      rateButtonsBlock.classList.add("rated");
      choosenBlock.classList.add("rated");
+     rateHeader.classList.add("deleted");
+     ratedText.classList.add("thanks")
      choosen_icon.innerHTML = button.innerHTML
      setTimeout(()=>{
 rateBlock.classList.remove("deleted")
