dbbeccb870b06e3c4f861c04e2e5d9327c1bba2d
Aramo98 <arammargaryan887@gmail.com>
2024-08-29 12:17:38 +0400

    save index

--- docs/js/chooseRate.js
+++ docs/js/chooseRate.js
@@ -3,9 +3,13 @@ const rateButtons = document.querySelectorAll(".rateBtn")
 const rateButtonsBlock = document.querySelector(".rateBtns")
 const choosenBlock = document.querySelector(".choosenBlock");
 const choosen_icon = document.querySelector(".choosen_icon")
-
-rateButtons.forEach((button)=>{
+const LS = localStorage
+const storageNum = 0
+LS.getItem("rateNumber")
+rateButtons.forEach((button,index)=>{
     button.addEventListener("click",()=>{
+    storageNum = index + 1
+    LS.setItem("rateNumber",storageNum)
      rateButtonsBlock.classList.add("rated");
      choosenBlock.classList.add("rated");
      choosen_icon.innerHTML = button.innerHTML
