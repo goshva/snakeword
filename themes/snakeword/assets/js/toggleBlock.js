0c9eeeceea255d8fd650ba8ec327754e53e58ff3
Aramo98 <arammargaryan887@gmail.com>
2024-10-14 08:41:01 +0400

    changes

--- themes/snakeword/assets/js/toggleBlock.js
+++ themes/snakeword/assets/js/toggleBlock.js
@@ -1,5 +1,8 @@
 const ThemeButton = document.querySelector(".ThemeButton");
 const topMenu = document.querySelector(".top-menu");
+const findedWordsBlock = document.querySelector(".findedWordsBlock")
+const lightRateBlock = document.querySelector(".rateBlock")
+const buttonBlock = document.querySelector(".buttonBlock");
   ThemeButton.addEventListener("click",()=>{
   if(localStorage.getItem("themes") === "light"){
     localStorage.removeItem("themes")
@@ -15,11 +18,17 @@ function changeDarkToLight(){
   if (localStorage.getItem("themes") === "light") {
     ThemeButton.classList.add("light")
     ThemeButton.innerHTML = '<i class="fa-regular fa-sun"></i>'
-    topMenu.classList.add(".light")
+    topMenu.classList.add("light")
+    findedWordsBlock.classList.add("light")
+    lightRateBlock.classList.add("light")
+    buttonBlock.classList.add("light")
   }else{
     ThemeButton.classList.remove("light")
-    topMenu.classList.remove(".light")
     ThemeButton.innerHTML = '<i class="fa-regular fa-moon"></i>'
+    topMenu.classList.remove("light")
+    findedWordsBlock.classList.remove("light")
+    lightRateBlock.classList.remove("light")
+    buttonBlock.classList.remove("light")
   }
 }
 
@@ -30,7 +39,7 @@ changeDarkToLight()
 // if click arrow icon open languages menu
 
 
-const buttonBlock = document.querySelector(".buttonBlock");
+
 buttonBlock.addEventListener("click",()=>{
   buttonBlock.classList.toggle("active")
   topMenu.classList.toggle("active")
